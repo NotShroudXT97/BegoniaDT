aa5e35d6ac7b23518fdda7b86262479994aa6b5d
NotShroudXT97 <NotShroudX97@GNUWeeb.Org>
2023-11-10 23:27:11 +0100

    Revert "begonia: Remove dead/unused product flags"
    This reverts commit 42c54216d5f13aeb83dea2fb295187e389ffe1c6.

--- octavi_begonia.mk
+++ octavi_begonia.mk
@@ -15,7 +15,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from begonia device
 $(call inherit-product, device/redmi/begonia/device.mk)
 
-<<<<<<< HEAD:octavi_begonia.mk
 # Inherit some common OctaviOS Stuff
 $(call inherit-product, vendor/octavi/config/common_full_phone.mk)
 TARGET_ENABLE_BLUR := true
@@ -24,10 +23,6 @@ USE_PIXEL_CHARGER := true
 USE_LEGACY_BOOTANIMATION := true
 WITH_GAPPS := true
 OCTAVI_MAINTAINER := NotShroudX97
-=======
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
->>>>>>> d715c43 (begonia: Remove dead/unused product flags):lineage_begonia.mk
 
 # Inherit some extras stuff
 $(call inherit-product-if-exists, vendor/extras/extras.mk)
